4ffb515b3a63ee8fbf9c6609b86ce829b578363f
Mazz1224 <mazz1224@gmail.com>
2018-12-21 07:40:35 -0500

    Under Construction

--- scripts/main.js
+++ scripts/main.js
@@ -1,12 +1,22 @@
+let length;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	length = width > height ? width : height;
 }
 
 function draw() {
-	background(255);
+	clear();
+
+	noStroke();
+	fill(255);
+	textAlign(CENTER, CENTER);
+	textSize(length / 15);
+	text('work in progress', width / 2, height / 2);
 }
 
 
 function windowResized() {
 	resizeCanvas(windowWidth, windowHeight, true);
+	length = width > height ? width : height;
 }
